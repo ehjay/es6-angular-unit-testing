31a2b158b73872eedc36a2274d11a9c383ea1cd8
Andrew Johnston <ehjay.github@gmail.com>
2016-05-29 16:31:32 -0400

    config babel and eslint to use es6

--- src/cat.spec.js
+++ src/cat.spec.js
@@ -1,10 +1,11 @@
-var cat = {
+const cat = {
   speak: function speak() {
     return "meow";
   }
-}
+};
 
-describe('cat', function() {
-  it('says meow', function() {
+describe("cat", function() {
+  it("says meow", function() {
+    expect(cat.speak()).toBe("meow");
   });
 });
